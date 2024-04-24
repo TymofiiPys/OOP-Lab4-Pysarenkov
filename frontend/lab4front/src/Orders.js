1b203ab88a791c513a7ff189b4f42bb1b02805e5
TymofiiPys <nfstimoha2004@gmail.com>
2024-04-24 10:16:08 +0300

    Show unpaid orders and send bills

--- frontend/lab4front/src/Orders.js
+++ frontend/lab4front/src/Orders.js
@@ -3,18 +3,29 @@ import axios from "axios";
 
 function Orders() {
     const [orders, setOrders] = useState([]);
-
+    const [unpaidOrders, setUnpaidOrders] = useState([]);
 
     useEffect(() => {
         // Fetch menu items from backend servlet API
         axios.get('/api/orders')
             .then(response => {
                 setOrders(response.data);
+                setUnpaidOrders(response.data.filter(order => order.status === 'ORDERED'));
             })
             .catch(error => {
                 console.error('Error fetching menu items:', error);
             });
     }, []);
+
+    const handleBillIssue = () => {
+        axios.put('/api/orders', unpaidOrders)
+            .then(response => {
+                console.log('Order updated successfully:', response.data);
+            })
+            .catch(error => {
+                console.error('Error placing order:', error);
+            });
+    }
 
     return (<div>
         <h2>Orders List</h2>
@@ -40,6 +51,30 @@ function Orders() {
             ))}
             </tbody>
         </table>
+        <div>
+            <h2>Unpaid Orders</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Client</th>
+                    <th>Menu Item</th>
+                    <th>Amount</th>
+                    <th>Cost</th>
+                </tr>
+                </thead>
+                <tbody>
+                {unpaidOrders.map(order => (
+                    <tr key={order.id}>
+                        <td>{order.client_name}</td>
+                        <td>{order.menu_item}</td>
+                        <td>{order.amount}</td>
+                        <td>${order.cost.toFixed(2)}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <button onClick={handleBillIssue}>Send bills</button>
+        </div>
     </div>);
 }
 
